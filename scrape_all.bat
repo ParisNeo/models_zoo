f8be4a8af362fa84a5104e29806338aded03b278
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-07 13:08:24 +0100

    new models

--- scrape_all.bat
+++ scrape_all.bat
@@ -6,12 +6,15 @@ python exl2_scrapper.py --name Bartowski
 
 python gguf_scrapper.py
 python gguf_scrapper.py --name mys
+python gguf_scrapper.py --name vilm
 python transformers_scraper.py --name llava-hf
 python transformers_scraper.py --nameA WizardLM
 python transformers_scraper.py --name Teknium
 python transformers_scraper.py --name NousResearch
 python transformers_scraper.py --name jondurbin
 python transformers_scraper.py --name Qwen
+python transformers_scraper.py --name vilm
+
 python gpt4all.py
 
 python open_ai_scraper.py
