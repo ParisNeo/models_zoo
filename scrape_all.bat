07579166947b6cfa5b5b3e4e70ec3d1701cf91f5
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-25 01:32:01 +0100

    new gptq models

--- scrape_all.bat
+++ scrape_all.bat
@@ -1,8 +1,11 @@
 python awq_scrapper.py
 python gptq_scrapper.py
+python gptq_scrapper.py --name LoneStriker
+
 python exl2_scrapper.py
 python exl2_scrapper.py --name turboderp
 python exl2_scrapper.py --name Bartowski
+python exl2_scrapper.py --name LoneStriker
 
 python gguf_scrapper.py
 python gguf_scrapper.py --name mys
@@ -10,6 +13,8 @@ python gguf_scrapper.py --name vilm
 python gguf_scrapper.py --name miqudev
 python gguf_scrapper.py --name cmp-nct
 python gguf_scrapper.py --name mlabonne
+python gguf_scrapper.py --name LoneStriker
+
 
 python transformers_scraper.py --name llava-hf
 python transformers_scraper.py --nameA WizardLM
@@ -20,6 +25,7 @@ python transformers_scraper.py --name Qwen
 python transformers_scraper.py --name vilm
 
 python transformers_scraper.py --name CroissantLLM
+
 
 python gpt4all.py
 
