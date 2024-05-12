0d74ef621db4aa5b12df282361267b1bed672e8e
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-05-12 21:21:39 +0200

    upgraded zoos

--- scrape_all.bat
+++ scrape_all.bat
@@ -1,13 +1,17 @@
 python awq_scrapper.py
 python awq_scrapper.py --name Bartowski
+python awq_scrapper.py --name HuggingFaceM4
+
 python gptq_scrapper.py
 python gptq_scrapper.py --name LoneStriker
 python gptq_scrapper.py --name Bartowski
+python gptq_scrapper.py --name HuggingFaceM4
+python gptq_scraper.py --name LoneStriker --type exl2
+
 
 python exl2_scraper.py
 python exl2_scraper.py --name turboderp
 python exl2_scraper.py --name Bartowski
-python gptq_scraper.py --name LoneStriker --type exl2
 
 python gguf_scrapper.py
 python gguf_scrapper.py --name mys
@@ -19,6 +23,7 @@ python gguf_scrapper.py --name LoneStriker
 python gguf_scrapper.py --name lmstudio-ai
 python gguf_scrapper.py --name MeetKai
 python gguf_scrapper.py --name Bartowski
+python gguf_scrapper.py --name HuggingFaceM4
 
 
 
@@ -37,6 +42,7 @@ python transformers_scraper.py --name Vezora
 
 python transformers_scraper.py --name CroissantLLM
 python transformers_scraper.py --name gradientai
+python transformers_scraper.py --name HuggingFaceM4
 
 
 
